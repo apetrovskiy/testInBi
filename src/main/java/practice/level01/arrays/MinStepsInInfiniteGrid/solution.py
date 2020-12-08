d5723cf2a63167a829a58ec769fd1981dae6ac58
Aleksandr Petrovskii <aleksandr_petrovskii@epam.com>
2020-12-08 14:22:03 +0300

    Update solution.py

--- src/main/java/practice/level01/arrays/MinStepsInInfiniteGrid/solution.py
+++ src/main/java/practice/level01/arrays/MinStepsInInfiniteGrid/solution.py
@@ -1,6 +1,6 @@
 class Solution:
-    	# @param A : list of integers
-	# @param B : list of integers
-	# @return an integer
-	def coverPoints(self, A, B):
-     return 0
+    # @param A : list of integers
+    # @param B : list of integers
+    # @return an integer
+    def coverPoints(self, A, B):
+        return 0
