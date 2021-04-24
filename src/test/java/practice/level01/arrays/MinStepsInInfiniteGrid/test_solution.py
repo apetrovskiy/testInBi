b9a54a9d7fdde22b61f33580e41c72213d541fd4
Alexander <apetrovskiy@hotmail.com>
2021-04-24 22:00:34 +0300

    fixes

--- src/test/java/practice/level01/arrays/MinStepsInInfiniteGrid/test_solution.py
+++ src/test/java/practice/level01/arrays/MinStepsInInfiniteGrid/test_solution.py
@@ -3,14 +3,14 @@ from src.main.java.practice.level01.arrays.MinStepsInInfiniteGrid.solution \
     import Solution
 
 
-@pytest.mark.parametrize(
-    "input_x,input_y,expected_result",
-    [
+test_data = [
         ([-2], [7], 0), ([-7, -13], [1, -5], 6),
         ([4, 8, -7, -5, -13, 9, -7, 8],
-         [4, -15, -10, -3, -13, 12, 8, -8], 108),
-    ],
-)
+         [4, -15, -10, -3, -13, 12, 8, -8], 108)
+]
+
+
+@pytest.mark.parametrize("input_x,input_y,expected_result", test_data)
 def test_solution_min_steps_in_grid(
         input_x: int, input_y: int, expected_result: int):
     assert expected_result == Solution().coverPoints(input_x, input_y)
